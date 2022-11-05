b5658b05ace7b46c1ef259d1da97c74d79f79039
patrickcarlos <57506968+patrickcarlos@users.noreply.github.com>
2022-11-05 19:38:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById("count-el")
+let saveEL = document.getElementById("save-el")
+let count = 0;
+ function increment() {
+    count +=1
+    countEl.innerText = count
+}
+function save(){
+    let countStr = count + " - "
+    saveEL.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
